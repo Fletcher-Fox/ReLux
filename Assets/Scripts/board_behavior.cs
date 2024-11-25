e346e32b6426199f3863e8ce0d91458f6336d1a3
Isaac Anthony Huerta <45578792+Fletcher-Fox@users.noreply.github.com>
2024-11-24 18:07:23 -0800

    Added basic space spawner
    Script to make instantiate spaces, simple WIP

---
+++ Assets/Scripts/board_behavior.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class board_behavior : MonoBehaviour
+{   
+
+    [SerializeField] private int length; // X value
+    [SerializeField] private int width; // Y value
+    [SerializeField] private GameObject prefab; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+        // X value
+        for (int i = 0; i < length; i++)
+        {
+            // Y value
+            for (int j = 0; j < width; j++)
+            {   
+                Vector3 temp_vector = new Vector3(i, 0, j);
+                Instantiate(prefab, temp_vector, Quaternion.identity);
+            }
+        }
+    }
+}
