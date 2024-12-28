2308ef45da773b90799a1186cc7af7118911127d
Isaac Anthony Huerta <45578792+Fletcher-Fox@users.noreply.github.com>
2024-12-28 03:19:11 -0800

    Created new scriptable objects

---
+++ Assets/Scripts/materials_SO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "materials_SO", menuName = "Scriptable Objects/materials_SO")]
+public class materials_SO : ScriptableObject
+{
+    public Material deafault_material;
+    public Material hover_material;
+    public Material select_material;
+}
