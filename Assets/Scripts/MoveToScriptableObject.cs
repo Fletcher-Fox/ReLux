9569af0696453b9e019565c1017a64f6e12964c3
Adam <AGCapdeville@gmail.com>
2024-12-27 22:23:36 -0800

    rotate movement transform by 45

--- Assets/Scripts/MoveToScriptableObject.cs
+++ Assets/Scripts/MoveToScriptableObject.cs
@@ -22,15 +22,20 @@ public class MoveToScriptableObject : MonoBehaviour
 
     void Update() {
         
-        // map to X & Z plane
         Vector3 m = new Vector3(moveInput.x, 0, moveInput.y);
 
         if (math.abs(m.x) > 0 || math.abs(m.z) > 0)
         {
-
-            m *= speed * Time.deltaTime;
+            m = m.normalized * speed * Time.deltaTime;
+            m = RotateWithQuaternion(m, 45);
             transform.Translate(m, Space.World);
         }
+    }
+
+    Vector3 RotateWithQuaternion(Vector3 vector, int rotation)
+    {
+        Quaternion q = Quaternion.Euler(0, rotation, 0); // 90 degrees around Y-axis
+        return q * vector;
     }
 
 }
