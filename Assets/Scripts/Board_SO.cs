459ea92caa8b737e2d32e61b977840426f4da163
Isaac Anthony Huerta <45578792+Fletcher-Fox@users.noreply.github.com>
2025-01-12 19:14:33 -0800

    Added some logic to keep selected spaces blue
    Current implementation keeps the spaces blue, however hovering over a blue spaces does not change its current material I don't know if this is how we want it to work

--- Assets/Scripts/Board_SO.cs
+++ Assets/Scripts/Board_SO.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Board_SO", menuName = "Scriptable Objects/Board_SO")]
@@ -5,6 +6,8 @@ public class Board_SO : ScriptableObject
 {
     [SerializeField] private space_SO space_event;
     [SerializeField] private materials_SO space_materials;
+
+    private List<GameObject> selected;
 
     void OnEnable()
     {
@@ -29,20 +32,32 @@ public class Board_SO : ScriptableObject
         if (space.GetComponent<MeshRenderer>().material.color !=  space_materials.select_material.color)
         {
             update_material(space, space_materials.select_material);
+            selected.Add(space);
+        }
+        else 
+        {
+            update_material(space, space_materials.deafault_material);
+            selected.Remove(space);
+        }
+    }
+
+    void onEnter(GameObject space)
+    {   
+        if (!selected.Contains(space))
+        {
+            update_material(space, space_materials.hover_material);
+        }
+    }
+
+    void onExit(GameObject space)
+    {   
+        if (selected.Contains(space))
+        {
+            update_material(space, space_materials.select_material); 
         }
         else 
         {
             update_material(space, space_materials.deafault_material);
         }
-    }
-
-    void onEnter(GameObject space)
-    {
-        update_material(space, space_materials.hover_material);      
-    }
-
-    void onExit(GameObject space)
-    {
-        update_material(space, space_materials.deafault_material); 
     }
 }
