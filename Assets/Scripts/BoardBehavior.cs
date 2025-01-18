f793731d0cd81d595aa0ff5041850ad9a36b4c3d
Isaac Anthony Huerta <45578792+Fletcher-Fox@users.noreply.github.com>
2025-01-17 18:26:34 -0800

    Changed the board behavior script to randomly choose a space prefab
    Board behavior scirpt now randomly chooses a space prefab to use when making the square board. Thought it might be cool to see what it would make

--- Assets/Scripts/BoardBehavior.cs
+++ Assets/Scripts/BoardBehavior.cs
@@ -1,12 +1,11 @@
 using System;
 using UnityEngine;
-
 public class BoardBehavior : MonoBehaviour
 {   
     
     [SerializeField] private IntegerSO length;
     [SerializeField] private IntegerSO width;
-    [SerializeField] private GameObject prefab; 
+    [SerializeField] private GameObject[] prefab; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +17,8 @@ public class BoardBehavior : MonoBehaviour
             for (int j = 0; j < width.Value; j++)
             {   
                 Vector3 temp_vector = new Vector3(i, 0, j);
-                GameObject space = Instantiate(prefab, temp_vector, Quaternion.identity);
+                int rand_Prefab = UnityEngine.Random.Range(0, prefab.Length - 1);
+                GameObject space = Instantiate(prefab[rand_Prefab], temp_vector, Quaternion.identity);
                 space.name = "Space_" + i + "_" + j;
             }
         }
