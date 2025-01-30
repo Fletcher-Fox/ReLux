3c41dfe4490cd52521bceedbd70da6d08d133589
Adam <AGCapdeville@gmail.com>
2025-01-29 20:54:01 -0800

    spawn WIP

--- Assets/Scripts/Scriptable Objects/MaterialsSO.cs
+++ Assets/Scripts/Scriptable Objects/MaterialsSO.cs
@@ -6,4 +6,5 @@ public class MaterialsSO : ScriptableObject
     public Material deafault_material;
     public Material hover_material;
     public Material select_material;
+    public Material spawn_material;
 }
--- Assets/Scripts/SpaceBehavior.cs
+++ Assets/Scripts/SpaceBehavior.cs
@@ -5,6 +5,16 @@ public class SpaceBehavior : MonoBehaviour
 {   
 
     [SerializeField] private SpaceSO space_event;
+    [SerializeField] private bool _isSpawnPoint;
+    [SerializeField] private MaterialsSO space_materials;
+
+    void Start() {
+        Renderer renderer = GetComponent<Renderer>(); // Get the Renderer component
+        if (renderer != null && _isSpawnPoint)
+        {
+            renderer.material = space_materials.spawn_material; // Set the material
+        }
+    }
 
     void OnMouseDown()
     {   
